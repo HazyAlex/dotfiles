06ff10566364e2d179b7ec5ce3ba9ad63dad7617
HazyAlex <HazyAlex@protonmail.com>
2022-10-14 20:17:11 +0100

    Add script to remove vscode splash screen icon

---
+++ Alex/AppData/Roaming/Code/User/vscode_remove_icon.bat
@@ -0,0 +1,9 @@
+del "C:\Users\Alex\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\workbench\browser\parts\editor\media\letterpress-dark.svg"
+
+del "C:\Users\Alex\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\workbench\browser\parts\editor\media\letterpress-hcDark.svg"
+
+del "C:\Users\Alex\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\workbench\browser\parts\editor\media\letterpress-light.svg"
+
+del "C:\Users\Alex\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\workbench\browser\parts\editor\media\letterpress-hcLight.svg"
+
+@pause
