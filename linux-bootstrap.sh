9fc7052005f56288097b266d5aef1be5dc7b656f
HazyAlex <HazyAlex@protonmail.com>
2025-09-29 22:49:06 +0100

    wip

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -25,18 +25,20 @@ fi
 cd ansible
 
 echo "Available tasks:"
+echo ""
 echo "1. Run all tasks"
 echo "2. Core server configuration"
 echo "3. Docker installation"
 echo "4. Git configuration"
+echo ""
 
 read -p "Enter your selection: " choice
 
 case $choice in
     1)
         ansible-playbook -i inventory/localhost.yml playbooks/base-server.yml
-        ansible-playbook -i inventory/localhost.yml playbooks/docker.yml
         ansible-playbook -i inventory/localhost.yml playbooks/git.yml
+        ansible-playbook -i inventory/localhost.yml playbooks/docker.yml
         ;;
     2)
         ansible-playbook -i inventory/localhost.yml playbooks/base-server.yml
